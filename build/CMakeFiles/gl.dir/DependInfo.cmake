fd82ac75c0b95aa64a9268d6ea7b7f97fd9b87f2
Benjamin McMullen <ben@Benjamins-MacBook-Air.local>
2024-04-07 12:58:42 -0400

    program compiles fine, but the TRIANGLE DOESNT FUCKING DISPLAY AGHHHAGHHAG

---
+++ build/CMakeFiles/gl.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ben/Documents/dev/Cpp/gl/gl-learn/src/glad.c" "CMakeFiles/gl.dir/src/glad.c.o" "gcc" "CMakeFiles/gl.dir/src/glad.c.o.d"
+  "/Users/ben/Documents/dev/Cpp/gl/gl-learn/src/main.cpp" "CMakeFiles/gl.dir/src/main.cpp.o" "gcc" "CMakeFiles/gl.dir/src/main.cpp.o.d"
+  "/Users/ben/Documents/dev/Cpp/gl/gl-learn/src/shader.cpp" "CMakeFiles/gl.dir/src/shader.cpp.o" "gcc" "CMakeFiles/gl.dir/src/shader.cpp.o.d"
+  "/Users/ben/Documents/dev/Cpp/gl/gl-learn/src/window.cpp" "CMakeFiles/gl.dir/src/window.cpp.o" "gcc" "CMakeFiles/gl.dir/src/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/gl.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gl.dir/src/glad.c.o"
+  "CMakeFiles/gl.dir/src/glad.c.o.d"
+  "CMakeFiles/gl.dir/src/main.cpp.o"
+  "CMakeFiles/gl.dir/src/main.cpp.o.d"
+  "CMakeFiles/gl.dir/src/shader.cpp.o"
+  "CMakeFiles/gl.dir/src/shader.cpp.o.d"
+  "CMakeFiles/gl.dir/src/window.cpp.o"
+  "CMakeFiles/gl.dir/src/window.cpp.o.d"
+  "gl"
+  "gl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/gl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
