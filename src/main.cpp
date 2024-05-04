babf23bc4aed84bf2b080a1a32039f8cb58270fc
pax <pax@pax.pax>
2024-05-04 09:50:36 -0400

    rewriting

--- src/main.cpp
+++ src/main.cpp
@@ -5,147 +5,54 @@
 #include <glad/glad.h>
 #include <spdlog/spdlog.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+// #include <glm/glm.hpp>
+// #include <glm/gtc/matrix_transform.hpp>
+// #include <glm/gtc/type_ptr.hpp>
 
 #include "constants.hpp"
 #include "shader.hpp"
 #include "window.hpp"
-#include "img.hpp"
 
-// https://docs.gl/
+// rewrite from scratch
 int main() {
   spdlog::set_level(spdlog::level::debug);
-  spdlog::info("project vrsn: {}", VERSION);
 
-  spdlog::debug("Setting up window..");
-  Win win(W_WIDTH, W_HEIGHT);
-  GLFWwindow *handle = win.getWinHandle();
-  win.setTitle("window");
+  Win window(W_WIDTH, W_HEIGHT);
+  GLFWwindow *handle = window.getWinHandle();
+  window.setTitle("window");
 
-  // load our resources
-  Shader shdr = Shader(
+  float verts[] = {
+    1.0, 0.5, 0.0,
+    -1.0, -1.0, 0.0,
+    -1.0, 1.0, 0.0
+  };
+
+  // gl pipeline
+  unsigned int VAO;
+  glGenBuffers(1, &VAO);
+  glBindVertexArray(VAO);
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, verts, GL_STATIC_DRAW);
+
+  Shader shdr(
     "./res/shdrs/vertexShader.glsl",
     "./res/shdrs/fragmentShader.glsl"
   );
 
-  // define our vertices (no it is not a square >:( )
-  float vertices[] = {
-    // vertices
-    0.0f, 0.75f, 0.0f,
-    -0.75f, -0.75f, 0.0f,
-    0.75f, -0.75f, 0.0f,
-
-    // colors
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f,
-    
-    // image textures
-    1.5f, 3.0f,
-    3.0f, 0.0f,
-    0.0, 0.0f,
-  };
-
-  // create VAO
-  unsigned int VAO;
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  // allocate mem and upload vertices to gpu
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // tell gpu how to parse pos vertices
+  // read the verts
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
   glEnableVertexAttribArray(0);
 
-  // now tell it how to parse color verts
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)(sizeof(float) * 9));
-  glEnableVertexAttribArray(1);
-
-  // now tell it how to parse the texture coords
-  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)(sizeof(float) * 18));
-  glEnableVertexAttribArray(2);
-
-  unsigned int wood_texture;
-  glGenTextures(1, &wood_texture);
-  glBindTexture(GL_TEXTURE_2D, wood_texture);
-
-  // configure image wrapping for both S and T axis (s+t correspond to x+y)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-
-  // set up how mipmaps are also filtered (im reusing code i thnk lmao ill deal withit later)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  Image wood = Image("./res/img/wood.jpg", true);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, wood.getWidth(), wood.getHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, wood.getData());
-  glGenerateMipmap(GL_TEXTURE_2D);
-
-  unsigned int well_texture;
-  glGenTextures(1, &well_texture);
-  glBindTexture(GL_TEXTURE_2D, well_texture);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  Image me = Image("./res/img/welly.jpg", true);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, me.getWidth(), me.getHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, me.getData());
-  glGenerateMipmap(GL_TEXTURE_2D);
-
-  glUseProgram(shdr.getShaderID());
-  glUniform1i(glGetUniformLocation(shdr.getShaderID(), "tex1"), 0);
-  glUniform1i(glGetUniformLocation(shdr.getShaderID(), "tex2"), 1);
-
-  // unbind all.
-  glBindVertexArray(0);
-  glBindBuffer(VBO, 0);
-  glUseProgram(0);
-
-
-  spdlog::debug("Entering mainloop");
+  bool epilespyToggle = true;
   while (!glfwWindowShouldClose(handle)) {
-    if (glfwGetKey(handle, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-      glfwSetWindowShouldClose(handle, true);
-    }
+    glfwPollEvents();
 
-    // transformations chapter.
-    glm::mat4 trans = glm::mat4(1.0f); // create identity matrix
-    trans = glm::rotate(trans, (float)glfwGetTime() * 2, glm::vec3(0.0, 0.0, 1.0));
-
-    // make background black
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-    // bind textures
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, wood_texture);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, well_texture);
-
-    // Render triangles
-    glUseProgram(shdr.getShaderID());
-    unsigned int transUniform = shdr.getUniform("trans");
-    glUniformMatrix4fv(transUniform, 1, GL_FALSE, glm::value_ptr(trans));
-
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    // unbind all
-    glUseProgram(0);
-    glBindVertexArray(0);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    // rendering..
 
     glfwSwapBuffers(handle);
     glfwPollEvents();
   }
-
-  glfwTerminate();
-  return 0;
 }
--- src/res/shdrs/fragmentShader.glsl
+++ src/res/shdrs/fragmentShader.glsl
@@ -1,19 +1,1 @@
 #version 330 core
-out vec4 FragColor;
-
-in vec3 ourColor;
-in vec2 TexCoord;
-
-uniform sampler2D tex1;
-uniform sampler2D tex2;
-
-void main() {
-  FragColor = mix(
-    texture(tex1, TexCoord),
-    texture(tex2, TexCoord) /* * vec4(ourColor, 1.0) */,
-    0.5
-  );
-
-  // FragColor = texture(tex1, TexCoord) *  vec4(ourColor, 1.0);
-  // FragColor = texture(tex2, TexCoord) *  vec4(ourColor, 1.0);
-}
